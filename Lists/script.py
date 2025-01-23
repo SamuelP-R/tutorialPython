022571e4f4c1525297be02c322a2c1b998d62717
Samuel <spineda@finkok.com.mx>
2025-01-23 09:02:57 -0600

    List

---
+++ Lists/script.py
@@ -0,0 +1,13 @@
+mylist = []
+mylist.append(1)
+mylist.append(2)
+mylist.append(3)
+mylist.append(4)
+mylist.append(5)
+print(mylist[0])
+print(mylist[1])
+print(mylist[2])
+
+print("Usamos un for")
+for x in mylist:
+    print(x)
