7d7a95df84ad04debad07d4a4c3a73b266aa0686
Samuelpr <spineda@finkok.com.mx>
2025-01-27 22:57:09 -0600

    22:54

--- Conditions/ejercicio.py
+++ Conditions/ejercicio.py
@@ -1,5 +1,24 @@
-
+#change this code
 number = 16
+second_number = ""
+first_array =[1,2,3]
+seconf_array = [1,2]
 
+if number > 15:
+    print("1")
 
+if first_array:
+    print("2")
 
+#la funcion len() se utiliza para obtener la longitud de un objeto o cadena 
+if len(seconf_array) == 2:
+    print("3")
+
+if len(first_array) + len(seconf_array) == 5:
+    print("4")
+
+if first_array and first_array[0] == 1:
+    print("5")
+
+if not second_number:
+    print("6")
---
+++ Loops/script.py
@@ -0,0 +1,9 @@
+"""
+Bucles(lops) hay dos tipos de bucles en python, for y while
+El bucle for 
+"""
+primes = [2,3,5,7,8]
+# in es un operador muy util que se utiliza para verificar si un elemento esta presente dentro de una secuencia, como una lista, una cadena, una tupla o un diccionario
+for prime in primes:
+    print(prime)
+
